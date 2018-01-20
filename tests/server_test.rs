8633321b4409e679ebbac04d4929a7787091ef1d
Andrea Chiumenti <achiumenti@red.software.systems>
2018-01-20 19:06:20 +0100

    more integration tests

--- tests/server_test.rs
+++ tests/server_test.rs
@@ -17,7 +17,7 @@ use hyper::server::Http;
 use hyper::Error;
 // use std::str::from_utf8;
 use std::vec::Vec;
-use futures::future::{ok};
+use futures::future::ok;
 
 mod sample_site;
 use sample_site::get_site_service;
@@ -50,48 +50,90 @@ fn do_get(handle: &Handle, port: u16, path: &str) -> FutureResponse {
     client.get(uri)
 }
 
-fn test_resource(page: &str, exp_status: StatusCode, exp_body: &str) {
+fn test_resource(payload: Vec<(&str, StatusCode, &str)>) {
     let (shutdown_tx, shutdown_rx) = future_channel();
     let (client_tx, client_rx) = mpsc::channel();
     let mut core = Core::new().unwrap();
 
     let socket_addr = serve(shutdown_rx);
     let hanlde = &core.handle();
-    core.run(do_get(hanlde, socket_addr.port(), page).and_then(|res| {
-        assert_eq!(res.status(), exp_status);
 
-        res.body().fold(Vec::new(), |mut v, chunk| {
-                v.extend(&chunk[..]);
-                ok::<_, Error>(v)
-            }).map(|chunks| {
-                let s = String::from_utf8(chunks).unwrap();
-                assert_eq!(s, exp_body);
-                shutdown_tx.send(true).unwrap();
-                client_tx.send(true).unwrap();
-            })
-    })).unwrap();
+    for (page, exp_status, exp_body) in payload {
+        core.run(do_get(hanlde, socket_addr.port(), page).and_then(|res| {
+            assert_eq!(res.status(), exp_status);
 
+            res.body()
+                .fold(Vec::new(), |mut v, chunk| {
+                    v.extend(&chunk[..]);
+                    ok::<_, Error>(v)
+                })
+                .map(|chunks| {
+                    let s = String::from_utf8(chunks).unwrap();
+                    assert_eq!(s, exp_body);
+                    client_tx.send(true).unwrap();
+                })
+        })).unwrap();
+    }
     client_rx.recv().unwrap();
-
+    shutdown_tx.send(true).unwrap();
 }
+//
+// fn test_resource(page: &str, exp_status: StatusCode, exp_body: &str) {
+//     let (shutdown_tx, shutdown_rx) = future_channel();
+//     let (client_tx, client_rx) = mpsc::channel();
+//     let mut core = Core::new().unwrap();
+//
+//     let socket_addr = serve(shutdown_rx);
+//     let hanlde = &core.handle();
+//     core.run(do_get(hanlde, socket_addr.port(), page).and_then(|res| {
+//         assert_eq!(res.status(), exp_status);
+//
+//         res.body()
+//             .fold(Vec::new(), |mut v, chunk| {
+//                 v.extend(&chunk[..]);
+//                 ok::<_, Error>(v)
+//             })
+//             .map(|chunks| {
+//                 let s = String::from_utf8(chunks).unwrap();
+//                 assert_eq!(s, exp_body);
+//                 shutdown_tx.send(true).unwrap();
+//                 client_tx.send(true).unwrap();
+//             })
+//     })).unwrap();
+//
+//     client_rx.recv().unwrap();
+// }
 
 #[test]
 fn test_page1() {
-    test_resource("page1", StatusCode::Ok, "page1");
+    test_resource(vec![("page1", StatusCode::Ok, "page1")]);
 }
 
 #[test]
 fn test_page2() {
-    test_resource("page2", StatusCode::Ok, "page2");
+    test_resource(vec![("page2", StatusCode::Ok, "page2")]);
 }
 
 #[test]
 fn test_page3() {
-    test_resource("page3", StatusCode::Ok, "page3");
+    test_resource(vec![("page3", StatusCode::Ok, "page3")]);
 }
 
 #[test]
 fn test_page_not_found() {
     let expected = format!("{}", StatusCode::NotFound.as_u16());
-    test_resource("notAValidPage", StatusCode::NotFound, expected.as_str());
+    test_resource(vec![
+        ("notAValidPage", StatusCode::NotFound, expected.as_str()),
+    ]);
+}
+
+#[test]
+fn test_page_multiple_resources() {
+    let expected = format!("{}", StatusCode::NotFound.as_u16());
+    test_resource(vec![
+        ("page3", StatusCode::Ok, "page3"),
+        ("page3", StatusCode::Ok, "page3"),
+        ("notAValidPage", StatusCode::NotFound, expected.as_str()),
+        ("page1", StatusCode::Ok, "page1"),
+    ]);
 }
